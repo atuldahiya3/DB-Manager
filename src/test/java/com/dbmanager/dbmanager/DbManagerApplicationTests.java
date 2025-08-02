a26feaea92b1ced3709665a14adcd812cb546852
atuldahiya3 <87331579+atuldahiya3@users.noreply.github.com>
2025-08-02 23:54:52 +0530

    initialize

---
+++ src/test/java/com/dbmanager/dbmanager/DbManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbmanager.dbmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
